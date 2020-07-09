1284ff905b1e6d1bbcb40c478274600dd14fca11
Linda <ltramos7@gmail.com>
2020-07-09 10:28:39 -0500

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,38 @@
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+let prevScrollpos = window.pageYOffset;
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+
+    showMenu = false;
+  }
+}
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav").style.top = "0";
+  } else {
+    document.querySelector(".nav").style.top = "-9rem";
+  }
+  prevScrollpos = currentScrollPos;
+};
